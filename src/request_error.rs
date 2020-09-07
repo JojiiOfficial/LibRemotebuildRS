d796f81ba7a8850f318f138a03356caffb29f83d
jojii <jojii@gmx.net>
2020-09-07 17:02:20 +0200

    impl std::error::Error trait for request_error

--- src/request_error.rs
+++ src/request_error.rs
@@ -9,3 +9,11 @@ pub enum Error {
     InvalidState,
     HTTPNotOk(u16),
 }
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl std::error::Error for Error {}
